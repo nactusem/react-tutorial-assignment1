b66b003dd6d54760f47d1e87f6f42c90383b68b7
mesutcan <mesutcan0632@hotmail.com>
2020-03-27 12:06:50 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserInput from "./UserInput";
+import UserOutput from "./UserOutput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    username: "mesutcan"
+  };
+
+  usernameHandler = (event) => {
+    this.setState({username:event.target.value})
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <UserInput changed={this.usernameHandler}/>
+          <UserOutput username={this.state.username}/>
+        </div>
+    );
+  }
 }
 
 export default App;
